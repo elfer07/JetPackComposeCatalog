c57f2f73a1106c32e7dd87fdd33defc6cdcc4f02
elfer07 <morenofernando@gmail.com>
2023-12-24 09:49:20 -0300

    Scaffold

--- app/src/main/java/com/example/jetpackcomposecatalogo/ui/scaffold/MyScaffold.kt
+++ app/src/main/java/com/example/jetpackcomposecatalogo/ui/scaffold/MyScaffold.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposecatalogo.ui.lists.MyList
 import kotlinx.coroutines.launch
@@ -131,4 +132,10 @@ fun MyDrawer(onCloseDrawer: () -> Unit) {
             .clickable { onCloseDrawer() }
         )
     }
+}
+
+@Preview
+@Composable
+fun MyScaffoldPreview() {
+    MyScaffold()
 }
